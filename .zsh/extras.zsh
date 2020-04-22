827b00560ba2a0e944949a045d588923ff27f469
Merlin Unterfinger <info@munterfinger.ch>
2020-04-22 23:34:23 +0200

    Correct mail.

--- .zsh/extras.zsh
+++ .zsh/extras.zsh
@@ -2,6 +2,6 @@
 GIT_AUTHOR_NAME="Merlin Unterfinger"
 GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 git config --global user.name "$GIT_AUTHOR_NAME"
-GIT_AUTHOR_EMAIL="info“munterfinger.ch”"
+GIT_AUTHOR_EMAIL="info@munterfinger.ch"
 GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
